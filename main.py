bafe85967a865aa8241f003c5a6999e2cb855422
Hollenite <ariyanpradhan112@gmail.com>
2025-12-30 18:01:21 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main():
+    path = "./books/frankenstein.txt"
+    book = get_book_text(path)
+    print(book)
+
+
+def get_book_text(path):
+    with open(path,encoding="utf-8") as f:
+        file_contents = f.read()
+        return file_contents
+
+
+main()
