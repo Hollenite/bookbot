d460a6f834250a3cc3a672a7c5fde8b6b2a0bc83
Hollenite <ariyanpradhan112@gmail.com>
2025-12-30 19:03:42 +0530

    commit

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from stats import word_count
-from stats import times
+from stats import *
 
 def main():
     path = "./books/frankenstein.txt"
-    book = get_book_text(path)
-    print(word_count(book))
-    print(times(book))
-
-
-def get_book_text(path):
-    with open(path,encoding="utf-8") as f:
-        file_contents = f.read()
-        return file_contents
+    printdata(path)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(path):
+    with open(path,encoding="utf-8") as f:
+        file_contents = f.read()
+        return file_contents
+
 def word_count(book):
     x  = len(book.split())
     return f"Found {x} total words"
@@ -12,3 +17,37 @@ def times(book):
             count[char] = 1
 
     return count
+
+def convert(dic):
+    val_list = []
+    for key, value in dic.items():
+        if key.isalpha():
+            x = {"char": key, "num": value}
+            val_list.append(x)
+    return val_list
+
+def sort_on(lis):
+    return lis["num"]
+
+def printdata(path):
+    path = path
+    book = get_book_text(path)
+    #this is now the entire dictionary in key,value pairs
+    occurence = times(book)
+    #this is now the unsorted list of each line of dictionary as a dictionary
+    list_val = convert(occurence)
+    #this is now the sorted list of dictionaries
+    list_val.sort(key=sort_on, reverse=True)
+    print(
+        f"============ BOOKBOT ============\n"
+        f"Analyzing book found at {path}...\n"
+        "----------- Word Count ----------\n"
+        f"Found {word_count(book)} total words\n"
+        "--------- Character Count -------\n"
+    )
+    for x in list_val:
+        print(f"{x['char']}: {x['num']}")
+    print(
+        "============= END ==============="
+    )
+
